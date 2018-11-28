c02949e05ef6f09cc9a80a821aae1d6249b2b54b
thomas.breleur@epitech.eu <thomas.breleur@epitech.eu>
2018-11-28 08:42:52 +0100

    Add new layer for movement and try new digit layout

--- keyboards/massdrop/ctrl/config.h
+++ keyboards/massdrop/ctrl/config.h
@@ -19,6 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
+#define AUTO_SHIFT_TIMEOUT 300
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID           0x04D8
 #define PRODUCT_ID          0xEED2
